dd6e26058f3665c40bac651faefd33951d951ad3
Bart Geurten <bgeurte@gwdg.de>
2021-03-23 23:05:35 +0100

    current state of corrections

--- test_CCAna.py
+++ test_CCAna.py
@@ -1,49 +1,78 @@
 from importlib import reload
-from counterCurrentAna import sortMultiFileFolder
+from counterCurrentAna import sortMultiFileFolder, matLabResultLoader
+import numpy as np
+from mediaHandler import mediaHandler
+
+
 #reload(sortMultiFileFolder)
 
 mff = sortMultiFileFolder('/media/gwdg-backup/BackUp/Vranda/data_counter_c-start/countercurrent_onefolder/rei_last_generation_11-2018') 
 fileDict = mff.__main__() 
 
-
-import numpy as np
-import scipy.io
 dataDict = fileDict['HMM2']
 
 # read arena box
 boxCoords = np.genfromtxt(dataDict['csv'],delimiter=',')      
-# read matlab analysis
-mat = scipy.io.loadmat(dataDict['anaMat'])
-metaData = mat['metaData']
-analysedData = mat['analysedData']
+
+# load matlab data
+matLabLoader = matLabResultLoader(dataDict['anaMat'])
+matLabLoader.getData()
+# load movie file
+mH = mediaHandler(dataDict['seq'],'norpix')
 
 
-traceResult      = analysedData[0][0][0]
+def calculateCoordShift(imageWidth,bufferShift):
+    xShift = int(bufferShift%imageWidth)  # modulo
+    yShift = bufferShift//imageWidth # integer division
+    return np.array([xShift,yShift])
 
-# traceInfo
-#
-# col  1: x-position in pixel
-# col  2: y-position in pixel
-# col  3: major axis length of the fitted ellipse
-# col  4: minor axis length of the fitted ellipse
-# col  5: ellipse angle in degree
-# col  6: quality of the fit
-# col  7: number of animals believed in their after final evaluation
-# col  8: number of animals in the ellipse according to surface area
-# col  9: number of animals in the ellipse according to contour length
-# col 10: is the animal close to an animal previously traced (1 == yes)
-# col 11: evaluation weighted mean
-# col 12: detection quality [aU] if
-# col 13: correction index, 1 if the area had to be corrected automatically
-traceInfo        = traceResult[:.0]
-traceContour     = traceResult[:,1]
-traceMidline     = traceResult[:,2]
-traceHead        = traceResult[:,3]
-traceTail        = traceResult[:,4]
+def shiftFrameCoords(head,tail,contour,midLine,coordShift):
+    head = head + coordShift
+    tail = tail + coordShift
+    contour = contour + coordShift
+    midLine = midLine + coordShift
+    return head, tail, contour, midLine
 
-trace            = analysedData[0][0][1]
-bendability      = analysedData[0][0][2]
-binnedBend       = analysedData[0][0][3]
-saccs            = analysedData[0][0][4]
-trigAveSacc      = analysedData[0][0][5]
-medMaxVelocities = analysedData[0][0][6] 
+
+def plotFrameOverlay(frame,midLine,head,tail,contour,boxCoords,coordShift):
+    head, tail, contour, midLine = shiftFrameCoords(head,tail,contour,midLine,coordShift)
+    plt.imshow(frame,cmap='gray')  
+    #plt.hold(True)
+    plt.plot(  midLine[:,0],midLine[:,1],'g.-')
+    plt.plot(       head[0],     head[1],'bo')
+    plt.plot(       tail[0],     tail[1],'bs')
+    plt.plot(  contour[:,0],contour[:,1],'y-')
+    plt.plot(boxCoords[:,0],boxCoords[:,1],'y-')
+    plt.plot(boxCoords[[0,-1],0],boxCoords[[0,-1],1],'y-')
+    plt.draw()
+
+def getFrameNo4Norpix(headerDict,frameNo,bonus):
+    return int(((frameNo+headerDict['origin'])%headerDict['allocated_frames'])+bonus)
+
+def loadNorPixFrame(mH,frameI,frameShift):
+    return mH.getFrame(getFrameNo4Norpix(mH.media.header_dict,frameI,frameShift))
+
+import matplotlib.pyplot as plt  
+
+contour = matLabLoader.traceContour
+head    = matLabLoader.traceHead
+tail    = matLabLoader.traceTail
+midLine = matLabLoader.traceMidline
+
+frameI  = 13258
+pixelShift = np.array([3,-12])
+frameShift = 0
+
+
+plt.ion()
+
+frame = loadNorPixFrame(mH,frameI,frameShift)
+plt.cla()
+plotFrameOverlay(frame,midLine[frameI],head[frameI,:],tail[frameI,:],contour[frameI],boxCoords,pixelShift)
+plt.pause(0.001)
+
+for frameI  in range(13000,14000):
+    frame = loadNorPixFrame(mH,frameI,frameShift)
+    plt.cla()
+    plotFrameOverlay(frame,midLine[frameI],head[frameI,:],tail[frameI,:],contour[frameI],boxCoords,pixelShift)
+    plt.pause(0.001)
