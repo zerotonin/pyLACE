1bf3edb6f6892b57e0ebfb004907fcfed0f0c3f4
Bart Geurten <bgeurte@gwdg.de>
2021-09-16 11:17:14 +0200

    small class to read simple spike 2 files

---
+++ spike2SimpleReader.py
@@ -0,0 +1,42 @@
+class spike2SimpleReader():
+    import neo
+
+    def __init__(self,fileName):
+        self.fileName      = fileName
+        self.eventList     = list()
+        self.analogSigList = list()
+        self.neoReader     = False
+        self.dataBlock     = False
+        self.outPutData    = list()
+    
+    def readByNeo(self):
+        self.neoReader = neo.io.Spike2IO(filename=self.fileName)
+        self.dataBlock = reader.read(lazy=False)[0]
+
+    def readSegments(self):
+        self.outPutData = list()
+        for seg in self.dataBlock.segments:
+            self.outPutData.append(self.readSingleSeg(seg))
+        
+
+    def readSingleSeg(self,seg):
+        events     = self.readEvents(seg)
+        analogSigs = self.readAnalogSignals(seg)
+        return (analogSigs,events)
+    
+    def readEvents(self,seg):
+        eventData = dict()
+        for event in seg.events:
+            eventData[event.name] = event.times
+        return eventData
+  
+
+    def readAnalogSignals(self,seg):
+        analogData = dict()
+        analogData['time_s']  = seg.analogsignals[0].times
+        for aSignal in seg.analogsignals:
+            analogData[aSignal.name] = aSignal.magnitude
+        return analogData
+
+
+
