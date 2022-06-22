f252b3d9a96db38e0cb34b4eafdf46fe5aab6f93
Bart Geurten <bgeurte@gwdg.de>
2022-06-22 17:43:57 +0200

    getting zen conform

--- fishDataBase.py
+++ fishDataBase.py
@@ -6,23 +6,23 @@ from tqdm import tqdm
 
 class fishDataBase():
 
-    def __init__(self,dataBasePath = '/media/gwdg-backup/BackUp/Zebrafish/pythonDatabase',dbPos = None):
-        self.dbPath  = dataBasePath
-        if dbPos == None:
-            self.dbPos = os.path.join(self.dbPath,'fishDataBase.csv')
+    def __init__(self,data_base_path = '/media/gwdg-backup/BackUp/Zebrafish/pythonDatabase',data_base_file_position = None):
+        self.database_path  = data_base_path
+        if data_base_file_position == None:
+            self.data_base_file_position = os.path.join(self.database_path,'fishDataBase.csv')
         else:
-            self.dbPos = dbPos
+            self.data_base_file_position = data_base_file_position
         self.loadDataBase()
 
     def loadDataBase(self):
         try:
-            self.dataBase = pd.read_csv(self.dbPos)
-            del self.dataBase['Unnamed: 0']
+            self.database = pd.read_csv(self.data_base_file_position)
+            del self.database['Unnamed: 0']
         except:
             answer ='?'
             while answer != 'y' and answer !='n':
-                print('Fish data base cannot be read at position: ' + str(self.dbPos))
-                answer = input('Do you want to create a fish data base at '+ str(self.dbPos)+ '? (y)es or (n)o' )
+                print('Fish data base cannot be read at position: ' + str(self.data_base_file_position))
+                answer = input('Do you want to create a fish data base at '+ str(self.data_base_file_position)+ '? (y)es or (n)o' )
             if answer == 'n':
                 raise ValueError('Cannot read fish data base')
             else:
@@ -36,15 +36,15 @@ class fishDataBase():
                   'path2_head_mm', 'path2_tail_mm', 'path2_probDensity', 
                   'path2_smr', 'path2_s2r', 'path2_seq', 'path2_csv', 
                   'path2_mat', 'path2_anaMat']
-        self.dataBase = pd.DataFrame([],columns=dataBaseFields)
-        self.dataBase.to_csv(self.dbPos)
+        self.database = pd.DataFrame([],columns=dataBaseFields)
+        self.database.to_csv(self.data_base_file_position)
         self.saveDataBase()
     
     def runMultiTraceFolder(self,folderPos,genName,expString,birthDate,startAt=0):
         mff = sortMultiFileFolder(folderPos,expString) 
         fileDict = mff.__main__()
         keys = [k for k in fileDict.keys()] 
-        allready_analysed_filenames = [os.path.basename(x) for x in self.dataBase.path2_anaMat]
+        allready_analysed_filenames = [os.path.basename(x) for x in self.database.path2_anaMat]
 
         for key in tqdm(keys[startAt::],desc='analyse files'):
             dataDict = fileDict[key]
@@ -60,7 +60,13 @@ class fishDataBase():
 
 
     def addDataBase(self,dbEntry):
-        self.dataBase  = self.dataBase.append(dbEntry,ignore_index=True)
+        self.database  = self.database.append(dbEntry,ignore_index=True)
+
+    def integrateDataBase(self,db_to_integrate_fPos):
+        # shorthand
+        new_df = pd.read_csv(db_to_integrate_fPos)
+        self.database  = self.database.append(new_df,ignore_index=True)
+    
     def saveDataBase(self):
-        print(self.dbPos)
-        self.dataBase.to_csv(self.dbPos)
+        print(self.data_base_file_position)
+        self.database.to_csv(self.data_base_file_position)
