5b4f877d34ecbd29a2bd3cb9b70f1ea778bf3503
Bart Geurten <bart.geurten@otago.ac.nz>
2023-04-21 15:20:18 +1200

    started to test c-start integration

--- run_scripts/run_spike2Scripts.py
+++ run_scripts/run_spike2Scripts.py
@@ -9,8 +9,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 import trace_analysis.SpikeDetector as SpikeDetector
+import fish_data_base.fishDataBase  as fishDataBase
 
-
+"""
 #fN = r'/home/bgeurten/cstart_experiments/sufge1/Homozygous/male/movie9/HmM10 II.smr'
 fN = r'/home/bgeurten/cstart_experiments/sufge1/IntWild/male/movie5/IntM6.smr'
 s2sr = spike2SimpleIO.spike2SimpleReader(fN)
@@ -19,6 +20,15 @@ segSav = spike2SimpleIO.segmentSaver(s2sr,'./testPanda.csv')
 df = segSav.main()[0]
 sd = SpikeDetector.SpikeDetector(df)
 spike_train_df = sd.main()
+"""
 
+#%%
+db = fishDataBase.fishDataBase("/home/bgeurten/fishDataBase",'/home/bgeurten/fishDataBase/fishDataBase_cruise.csv')
+db.rebase_paths()
+
+
+multiFileFolder = '/home/bgeurten/cstart_experiments/sufge1/'
+# Experiment types CCur counter current , Ta tapped, Unt untapped, cst, c-startz
+db.run_multi_trace_folder(multiFileFolder,'sufge1','cst','08-2019',start_at=0)
 
 
