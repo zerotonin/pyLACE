2066d18fe31ee3324a4820b1231afe12755b6286
Bart Geurten <bart.geurten@otago.ac.nz>
2023-04-21 14:21:32 +1200

    please go on

--- fish_data_base/fishRecAnalysis.py
+++ fish_data_base/fishRecAnalysis.py
@@ -82,6 +82,25 @@ class fishRecAnalysis():
     """
     
     def __init__(self,dataDict,genName,expStr,birthDate,dataBasePath = '/media/gwdg-backup/BackUp/Zebrafish/pythonDatabase'):
+        """
+        Initializes the class with the given parameters.
+        
+        Args:
+            dataDict (dict): A dictionary containing the data related to the trace files.
+            genName (str): The gene name associated with the trace files.
+            expStr (str): The name of the experiment associated with the trace files.
+            birthDate (str): The birth date of the fish associated with the trace files.
+            dataBasePath (str, optional): The path to the folder containing the database files. 
+                                        Defaults to '/media/gwdg-backup/BackUp/Zebrafish/pythonDatabase'.
+        
+        Attributes:
+            dbPath (str): The path to the folder containing the database files.
+            dataDict (dict): A dictionary containing the data related to the trace files.
+            genName (str): The gene name associated with the trace files.
+            expStr (str): The name of the experiment associated with the trace files.
+            arena_sizes (dict): A dictionary containing the known arena sizes for different experiment types.
+                                The key is the experiment type, and the value is a tuple (y, x) representing the size.
+        """
         self.dbPath   = dataBasePath
         self.dataDict = dataDict
         self.genName  = genName
@@ -93,6 +112,9 @@ class fishRecAnalysis():
         self.arena_sizes = {'cruise':(114,248),'c_start':(40,80),'counter_current':(45,167)}
     
     def makeSaveFolder(self):
+        """
+        Creates a new folder for saving the analyzed data using a formatted folder name.
+        """
         recNumber = len([os.path.join(self.dbPath, o) for o in os.listdir(self.dbPath)  if os.path.isdir(os.path.join(self.dbPath,o))])
         folderName = '{}_{}_{}_{}_{}_ID#{}'.format(self.expStr,self.dataDict['genotype'],self.dataDict['birthDate'],self.dataDict['sex'],self.dataDict['animalNo'],recNumber)
         self.savePath = os.path.join(self.dbPath,folderName)
@@ -100,6 +122,9 @@ class fishRecAnalysis():
         #return folderName
 
     def correctionAnalysis(self):
+        """
+        Performs the correction and analysis of the trace data.
+        """        
         self.traCor = traceCorrector(self.dataDict)
         # calibrate the movie if nescessary
         if self.traCor.mmTraceAvailable == False:
@@ -121,17 +146,51 @@ class fishRecAnalysis():
 
 
     def prepDf_3D(self,col1Name,col2Name,reps):
+        """
+        Prepares an empty DataFrame with columns for 3D data.
+        
+        Args:
+            col1Name (str): The prefix for the first set of column names.
+            col2Name (str): The prefix for the second set of column names.
+            reps (int): The number of repetitions in the data.
+        
+        Returns:
+            pd.DataFrame: An empty DataFrame with the prepared columns.
+            list: A list of the created column names.
+        """
         columnLabels = np.linspace(0,reps-1,reps,dtype=int)
         columnLabels = [[col1Name+'_'+str(x),col2Name+'_'+str(x)] for x in columnLabels]
         columnLabels = [j for sub in columnLabels for j in sub]
         return pd.DataFrame([],columns=columnLabels),columnLabels
 
     def getTimeIndex(self,dataDF):
+        """
+        Converts the DataFrame index from frames to time in seconds.
+        
+        Args:
+            dataDF (pd.DataFrame): The DataFrame with frame-based index.
+        
+        Returns:
+            pd.DataFrame: The DataFrame with time-based index.
+        """
         dataDF.index= dataDF.index/self.traAna.fps
         dataDF.index.name = 'time sec'
         return dataDF #np.linspace(0,self.traAna.traceLenSec,self.traAna.traceLenFrame)
 
     def makePandasDF_3D(self,data,col1Name,col2Name,index=None):
+        """
+        Creates a 3D pandas DataFrame from the given data.
+
+        Args:
+            data (list): The list containing the data to be converted into a DataFrame.
+            col1Name (str): The prefix for the first set of column names.
+            col2Name (str): The prefix for the second set of column names.
+            index (str, optional): The index type for the DataFrame. Set to 'Time' to use time-based index.
+                                Defaults to None.
+
+        Returns:
+            pd.DataFrame: The created pandas DataFrame.
+        """
         reps = max([len(entry) for entry in data])
         dataDF,colLabels = self.prepDf_3D(col1Name,col2Name,reps)
         for  detection in data:
@@ -144,13 +203,35 @@ class fishRecAnalysis():
         return dataDF
 
     def makePandasDF_2D(self,data,col1Name,col2Name,index=None):
+        """
+        Creates a 2D pandas DataFrame from the given data.
+
+        Args:
+            data (list): The list containing the data to be converted into a DataFrame.
+            col1Name (str): The name for the first column.
+            col2Name (str): The name for the second column.
+            index (str, optional): The index type for the DataFrame. Set to 'Time' to use time-based index.
+                                    Defaults to None.
+
+        Returns:
+            pd.DataFrame: The created pandas DataFrame.
+        """
         dataDF = pd.DataFrame(data,columns= [col1Name,col2Name])
         if index == 'Time':
             dataDF = self.getTimeIndex(dataDF)
         return dataDF
        
     def makePandasDF4Hist(self,data):
-   
+        """
+        Creates a pandas DataFrame for histogram data.
+
+        Args:
+            data (np.array): The histogram data to be converted into a DataFrame.
+
+        Returns:
+            pd.DataFrame: The created pandas DataFrame.
+        """
+    
         streamAxis = np.linspace(0,162,data.shape[1]+2)
         orthoAxis  = np.linspace(0,43,data.shape[0]+2)
         columnLabels = ['streamAxisMM ' +str(int(x)) for x in streamAxis[1:-1]]
@@ -162,6 +243,12 @@ class fishRecAnalysis():
 
 
     def makeResultDFs(self):
+        """
+        Creates a dictionary of pandas DataFrames for the analyzed data.
+
+        Returns:
+            dict: A dictionary containing the resulting DataFrames.
+        """
         returnDict = {'inZoneBendability': None,'midLineUniform_mm': None,
                 'midLineUniform_pix': None,'head_mm': None,
                 'tail_mm': None,'probDensity': None,'trace_mm':None}
@@ -185,6 +272,12 @@ class fishRecAnalysis():
         return returnDict
 
     def saveDataFrames(self):
+        """
+        Saves the analyzed data as DataFrames in CSV format.
+
+        Returns:
+            dict: A dictionary containing the database entry for the saved data.
+        """
         dataFrames = self.makeResultDFs()
         self.makeSaveFolder()
         for key in dataFrames.keys():
@@ -196,6 +289,12 @@ class fishRecAnalysis():
         return dbEntry
 
     def makeDataBaseEntry(self):
+        """
+        Creates a dictionary containing the database entry for the analyzed data.
+
+        Returns:
+            dict: The dictionary containing the database entry.
+        """
         dbEntry = self.dataDict.copy()
         del dbEntry['movieFrameIDX']
         del dbEntry['probDensity_xCenters']
@@ -215,6 +314,12 @@ class fishRecAnalysis():
         return dbEntry
 
     def save2DMatrix(self,dataListEntry):
+        """
+        Saves a 2D matrix from the dataListEntry to a text file.
+
+        Args:
+            dataListEntry (list): The list containing the tag and the 2D matrix to be saved.
+        """
 
         tag = dataListEntry[0]
         mat = dataListEntry[1]
