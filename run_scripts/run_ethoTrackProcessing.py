40c366382dcc4d3bc6ed128aca3ae0bef5ef1b1b
Bart Geurten <bart.geurten@otago.ac.nz>
2023-05-04 10:33:52 +1200

    more testing

--- run_scripts/run_ethoTrackProcessing.py
+++ run_scripts/run_ethoTrackProcessing.py
@@ -18,7 +18,7 @@ if ev_db.db_connection:
     subject_df = ev_db.get_data_for_subject(tank_numbers,id_val )
     print(subject_df)
     ev_dp = EthovisionDataProcessor(subject_df)
-    result_df = ev_dp.process_data()
+    result_df,histograms = ev_dp.process_data(tank_height=20.5,tank_width=20.5)
     print(result_df)
     # Close the SQLite database connection
     ev_db.close_connection()
