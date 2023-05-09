7c9530637e3ff3017e6bd91bee003b87264530c4
Bart Geurten <bart.geurten@otago.ac.nz>
2023-05-09 15:12:31 +1200

    got the day wise analysis as its on function

--- run_scripts/run_ethoTrackProcessing.py
+++ run_scripts/run_ethoTrackProcessing.py
@@ -1,4 +1,5 @@
 from data_base_analyser.EthoVisionExperimentSeries import EthoVisionExperimentSeries
+from plotting.DaywiseAnalysis import DaywiseAnalysis
 import os
 import pandas as pd
 import numpy as np
@@ -9,45 +10,22 @@ import scipy.ndimage
 from matplotlib.colors import LogNorm
 import matplotlib.cm as cm
 
+
+
 #[print(x.shape) for x in histograms]
 # Usage
 tag = 'habituation2023'
 parent_directory = '/home/bgeurten/ethoVision_database/'
 
 # Compile data daywise
-etho_vision_analysis = EthoVisionExperimentSeries(tag, parent_directory)
-etho_vision_analysis.process_and_save()
+#etho_vision_analysis = EthoVisionExperimentSeries(tag, parent_directory)
+#etho_vision_analysis.process_and_save()
 
 db_position = f'{parent_directory}{tag}_daywise_analysis.csv'
 df = pd.read_csv(db_position)
 
-histogram_file_positions = find_npy_files(parent_directory)
-
-fishID, hists = load_normed_histograms(histogram_file_positions)
-
-
-male_hists,female_hists = sort_hists_by_sex(hists,fishID,df)
-male_hists = normalise_histograms(np.nanmedian(male_hists,axis=3))
-female_hists = normalise_histograms(np.nanmedian(female_hists,axis=3))
-
-create_daywise_histograms(male_hists)
-create_daywise_histograms(female_hists)
-
-for topic in ['Median_speed_cmPs', 'Gross_speed_cmPs',
-       'Median_activity_duration_s', 'Activity_fraction',
-       'Median_freezing_duration_s', 'Freezing_fraction',
-       'Median_top_duration_s', 'Top_fraction', 'Median_bottom_duration_s',
-       'Bottom_fraction', 'Median_tigmotaxis_duration_s',
-       'Tigmotaxis_fraction', 'Tigmotaxis_transitions', 'Latency_to_top_s',
-       'Distance_travelled_cm']:
-    
-    create_vertical_box_stripplot(df,'Day_number',topic,'Sex',('M','F'))
-    plt.show()
-
-
-create_vertical_box_stripplot(df,'Day_number','Top_fraction','Sex',('M','F'))
-plt.show()
-
+dwa= DaywiseAnalysis(df)
+dwa.run()
 
 
 def create_top_fraction_lineplot(df,measure):
